ba15da58ec62eda42843b0b7593e311d78a71587
Naïmsdi <naimoudinesaidi@gmail.com>
2024-06-02 21:08:04 +0200

    fix searchbar input color

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import SearchBarModal from './components/SearchBarModal'
